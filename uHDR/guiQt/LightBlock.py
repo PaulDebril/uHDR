cb2b4e9843e3172866b5892e7c8a10cc201f93c8
woorzz <marinelangrez@outlook.fr>
2024-06-11 22:03:05 +0200

    fix(exposure) : add contrast and curve

--- uHDR/guiQt/LightBlock.py
+++ uHDR/guiQt/LightBlock.py
@@ -2,6 +2,10 @@
 
 from PyQt6.QtWidgets import QFrame, QVBoxLayout, QSlider, QLabel
 from PyQt6.QtCore import pyqtSignal, Qt
+from guiQt.AdvanceSlider import AdvanceSlider
+from guiQt.Contrast import Contrast
+from guiQt.CurveWidget import CurveWidget
+from guiQt.MemoGroup import MemoGroup
 
 class LightBlock(QFrame):
     exposureChanged = pyqtSignal(float)
@@ -10,17 +14,26 @@ class LightBlock(QFrame):
         super().__init__()
         self.setFrameShape(QFrame.Shape.StyledPanel)
 
-        self.topLayout = QVBoxLayout()
+        # attributes
+        self.active : bool = True
+
+        # layout and widgets
+        self.topLayout : QVBoxLayout = QVBoxLayout()
         self.setLayout(self.topLayout)
 
-        self.exposureSlider = QSlider(Qt.Orientation.Horizontal)
-        self.exposureSlider.setRange(-30, 30)  # Plage de -30 à +30 EV
-        self.exposureSlider.setValue(0)  # Valeur par défaut
-        self.exposureSlider.valueChanged.connect(self.emitExposureChanged)
 
+        self.exposure : AdvanceSlider =AdvanceSlider('exposure',0.0,(-30,+30),(-3.0,+3.0),10)
         self.exposureValueLabel = QLabel("Exposure: 0.0")
-        self.topLayout.addWidget(self.exposureSlider)
-        self.topLayout.addWidget(self.exposureValueLabel)
+        self.exposure.valueChanged.connect(self.emitExposureChanged)
+        self.contrast :  Contrast =Contrast()
+        self.curve :  CurveWidget =CurveWidget()
+        #self.memory : MemoGroup = MemoGroup()
+
+        ## add to layout
+        self.topLayout.addWidget(self.exposure)
+        self.topLayout.addWidget(self.contrast)
+        self.topLayout.addWidget(self.curve)
+        #self.topLayout.addWidget(self.memory)
 
     def emitExposureChanged(self, value):
         ev_value = value / 10.0  # Ajuster l'échelle si nécessaire
